a90cc0e425f0f3f44b850a252685a65adbdb0e12
Pablo Cantarin <pablo.cantarin86@gmail.com>
2025-03-16 18:52:45 -0300

    seguimos probando

--- render-build.sh
+++ render-build.sh
@@ -10,17 +10,19 @@ npm install
 
 # Ensure the Puppeteer cache directory exist
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+PROJECT_CACHE_DIR=/opt/render/shdaowFitDesarrollo/src/.cache/puppeteer/chrome
 mkdir -p $PUPPETEER_CACHE_DIR
+mkdir -p $PROJECT_CACHE_DIR
 
 # Install Puppeteer and download Chrome
 npx puppeteer browsers install chrome
 
 # Store/pull Puppeteer cache with build cache
-if [[! -d $PUPPETEER_CACHE_DIR]]; then
-    echo  "...Copying Puppeteer Cache from Build Cache"
-    # Copying from the actual path where Puppeteer stores is Chrome binary
-    cp -R /opt/render/shadowFitDesarrollo/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+if [[ ! -d $PROJECT_CACHE_DIR ]]; then
+    echo "...Copying Puppeteer Cache from Build Cache"
+    # Copying Puppeteer browser files to your project's cache directory
+    cp -R $PUPPETEER_CACHE_DIR/chrome/* $PROJECT_CACHE_DIR
 else
     echo "...Storing Puppeteer Cache in Build Cache"
-    cp -R $PUPPETEER_CACHE_DIR /opt/render/shadowFitDesarrollo/src/.cache/puppeteer/chrome/
+    cp -R $PROJECT_CACHE_DIR/* $PUPPETEER_CACHE_DIR
 fi
