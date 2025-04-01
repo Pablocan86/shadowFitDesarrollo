59578b0e99c5b44a83eafee476e68352078872b8
Pablocan86 <pablo.cantarin86@gmail.com>
2025-04-01 09:01:16 -0300

    se mejoran estilos responsive

--- src/public/js/panelAlumno.js
+++ src/public/js/panelAlumno.js
@@ -15,3 +15,24 @@ fetch(`/api/users/ver-foto-perfil-alumno/${idAlumno}`)
   .catch((error) => {
     console.error("Error al obtener la imagen de perfil:", error);
   });
+
+document.querySelectorAll("#listaProgresos img").forEach((img, index) => {
+  const idUnico = Date.now() * 1000 + index;
+  img.id = `${idUnico}`;
+  const nombreArchivo = img.getAttribute("name");
+  if (nombreArchivo) {
+    fetch(`/api/users/fotoprogreso/${idAlumno}/${nombreArchivo}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // La URL está en data.url
+        const imageUrl = data.url;
+
+        // Actualizar el atributo src de la imagen
+        const imgElement = document.getElementById(`${idUnico}`);
+        imgElement.src = imageUrl;
+      })
+      .catch((error) => {
+        console.error("Error al obtener la imagen de perfil:", error);
+      });
+  }
+});
